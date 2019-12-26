8e30bcd85e2289bf90871e6174c15670a3e2cca4
MaciejZientara <mtzientara@gmail.com>
2019-12-26 10:16:49 -0800

    dzialajacy LandS

--- ladder_snake.c
+++ ladder_snake.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+//#include <unisted.h>
 
 #define N 10
 
@@ -17,8 +18,12 @@ GtkWidget *IleGraczy;
 int PozGracz[3][2];
 int tura=0,ILE;
 
+bool koniec=false;
+bool tabgoal[N*N+1];
+
 static struct parametry{
-	char *nr;
+	//bool goal;
+	int nr;
 	bool isWall;
 	bool gracz[3];
 	int ladder;
@@ -43,11 +48,11 @@ static void update(){
 				else
 					c=' ';
 				int tmpp;
-				if(i%2==1)
-					tmpp=N*(N-i)+(N-j+1);
-				else	
-					tmpp=N*(N-i)+j;
-				char *tmp=g_strdup_printf("%d\n%c%c%c",tmpp,a,b,c);
+		//		if(i%2==1)
+		//			tmpp=N*(N-i)+(N-j+1);
+		//		else	
+		//			tmpp=N*(N-i)+j;
+				char *tmp=g_strdup_printf("%d\n%c%c%c",tab[i][j].nr,a,b,c);
 				//printf("%s\n",tmp);
 				gtk_button_set_label((GtkButton*)TabButton[i][j],tmp);
 			}
@@ -99,9 +104,9 @@ static void InitPlansza(){
 				tab[i][j].isWall=true;
 			else{
 				if(i%2==1)
-					tab[i][j].nr=g_strdup_printf("%d",N*(N-i)+(N-j+1));
+					tab[i][j].nr=N*(N-i)+(N-j+1);
 				else	
-					tab[i][j].nr=g_strdup_printf("%d",N*(N-i)+j);
+					tab[i][j].nr=N*(N-i)+j;
 			}
 		}
 /* //debug
@@ -137,7 +142,7 @@ static void InitPlansza(){
 					c='Z';
 				else
 					c=' ';
-				char *tmp=g_strdup_printf("%s\n%c%c%c",tab[i][j].nr,a,b,c);
+				char *tmp=g_strdup_printf("%d\n%c%c%c",tab[i][j].nr,a,b,c);
 				TabButton[i][j]=gtk_button_new_with_label(tmp);
 			}
 			gtk_grid_attach(GTK_GRID(plansza),TabButton[i][j],j,i,1,1);
@@ -145,6 +150,7 @@ static void InitPlansza(){
 }
 
 static void DiceRoll(){
+	if(koniec)return;
 	srand(time(0));
 	int tmp=rand()%6+1;
 	free(P);
@@ -161,6 +167,7 @@ static void DiceRoll(){
 	for(int q=0; q<tmp; q++){
 		int i=PozGracz[tura][0];
 		int j=PozGracz[tura][1];
+		if((N*N)-tab[i][j].nr<tmp)break;
 		if(i%2==0){
 			if(j==N){
 				tab[i][j].gracz[tura]=false;
@@ -185,15 +192,166 @@ static void DiceRoll(){
 				PozGracz[tura][1]--;
 			}	
 		}
+		//usleep(500);
 		update();
 	}
-	//waz/drabina
-	//losowanie waz/drabina N razy	
-
-
-	//if(win)...
+	int x=PozGracz[tura][0],y=PozGracz[tura][1];
+	if(tab[x][y].ladder){
+		free(P);
+		P=calloc(sizeof(struct pairGS),1);
+		P->G=LS;
+		strcpy(P->S,"LADDER!");
+		dialog();
+		int xp=x,yp=y,len=tab[x][y].ladder;
+		for(int i=0; i<len; i++)
+			if(xp%2==0){
+				if(yp==N)
+					xp--;
+				else
+					yp++;	
+			}
+			else{
+				if(yp==1)
+					xp--;
+				else
+					yp--;	
+			}
+				tab[x][y].gracz[tura]=false;
+				tab[xp][yp].gracz[tura]=true;
+				PozGracz[tura][0]=xp;
+				PozGracz[tura][1]=yp;
+	}
+	
+	if(tab[x][y].snake){
+		free(P);
+		P=calloc(sizeof(struct pairGS),1);
+		P->G=LS;
+		strcpy(P->S,"SNAKE!");
+		dialog();
+		int xp=x,yp=y,len=tab[x][y].snake;
+		for(int i=0; i<len; i++)
+			if(xp%2==0){
+				if(yp==1)
+					xp++;
+				else
+					yp--;	
+			}
+			else{
+				if(yp==N)
+					xp++;
+				else
+					yp++;	
+			}
+				tab[x][y].gracz[tura]=false;
+				tab[xp][yp].gracz[tura]=true;
+				PozGracz[tura][0]=xp;
+				PozGracz[tura][1]=yp;
+	}
+	update();
+	if(PozGracz[tura][0]==1 && PozGracz[tura][1]==1){
+		free(P);
+		P=calloc(sizeof(struct pairGS),1);
+		P->G=LS;
+		if(tura==0)
+			strcpy(P->S,g_strdup_printf("X WON!"));
+		if(tura==1)
+			strcpy(P->S,g_strdup_printf("Y WON!"));
+		if(tura==2)
+			strcpy(P->S,g_strdup_printf("Z WON!"));
+		dialog();
+		koniec=true;
+	}
 	tura=(tura+1)%ILE;
 	return;
+}
+
+static void gen(){
+	srand(time(0));
+	tabgoal[1]=true;
+	tabgoal[N*N]=true;
+	for(int q=0; q<N; q++){
+		int x=rand()%N+1,y=rand()%N+1;
+		while(tabgoal[tab[x][y].nr]){
+			//printf("dupa1\n");
+			x=rand()%N+1;
+			y=rand()%N+1;
+		}
+		tabgoal[tab[x][y].nr]=true;
+		int maxlen=(N*N)-tab[x][y].nr-2;
+		int xp=x,yp=y,len=0;
+		while(tabgoal[tab[xp][yp].nr+len]){
+			len=rand()%maxlen+1;
+		}
+			for(int i=0; i<len; i++)
+				if(xp%2==0){
+					if(yp==N)
+						xp--;
+					else
+						yp++;	
+				}
+				else{
+					if(yp==1)
+						xp--;
+					else
+						yp--;	
+				}
+		
+		tab[x][y].ladder=len;
+		tabgoal[tab[xp][yp].nr]=true;
+	}
+	for(int q=0; q<N; q++){
+		int x=rand()%N+1,y=rand()%N+1;
+		while(tabgoal[tab[x][y].nr]){
+			//printf("dupa2\n");
+			x=rand()%N+1;
+			y=rand()%N+1;
+		}
+		tabgoal[tab[x][y].nr]=true;
+		int maxlen=tab[x][y].nr-2;
+		int xp=x,yp=y,len=0;
+		while(tabgoal[tab[xp][yp].nr-len]){
+			len=rand()%maxlen+1;
+		}
+		//printf("max=%d,len=%d,nr=%d\n",maxlen,len,tab[x][y].nr);
+			for(int i=0; i<len; i++)
+				if(xp%2==0){
+					if(yp==1)
+						xp++;
+					else
+						yp--;	
+				}
+				else{
+					if(yp==N)
+						xp++;
+					else
+						yp++;	
+				}
+		//printf("nr=%d,len=%d,nrg=%d\n",tab[x][y].nr,len,tab[xp][yp].nr);
+		//printf("dla snake tab[%d][%d] goal[%d][%d]\n",x,y,xp,yp);
+		tab[x][y].snake=len;
+		tabgoal[tab[xp][yp].nr]=true;
+	}
+/* //debug
+	int ilerazy=0,ilesnake=0,ileladder=0;
+	for(int i=1; i<(N+1); i++){
+		for(int j=1; j<(N+1); j++){
+			//if(tab[i][j].isWall)
+			//	printf("@ ");
+			//else{
+				//if(i%2==1)
+				if(tabgoal[tab[i][j].nr]){
+					printf("%d-s=%d-l=%d ",tab[i][j].nr,tab[i][j].snake,tab[i][j].ladder);
+					ilerazy++;
+					if(tab[i][j].snake)ilesnake++;else if(tab[i][j].ladder)ileladder++;
+					}
+				//else	
+				//	printf("%d ",tab[i][j].nr);
+			//}
+		}
+	printf("\n");
+	}
+	printf("ilerazy=%d\nilesnake=%d\nileladder=%d\n",ilerazy,ilesnake,ileladder);
+*/	
 }
 
 static GtkWidget* gra(){
@@ -250,19 +408,7 @@ static GtkWidget* gra(){
 	//gtk_grid_attach(GTK_GRID(LS),trzy,0,3,1,1);	
 	gtk_box_pack_start((GtkBox*)IleGraczy,trzy,TRUE,FALSE,0);
 	
-
-	//grid na box, potem widget_hide, object_ref, container_remove i add nowy
-
-	//update, dialog, update, no box
-	
-	//wybor ile graczy
-
-	//wyswietl plansze i roll button
-
-	//dialog na ile oczek na kostce
-
-	//jak plansza???????
-
+	gen();	
 
 	return LS;
 }
